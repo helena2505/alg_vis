2a013c906f5967a14813c63c925bc3a2e8fb03e6
Tanya <tanyashorygina@gmail.com>
2019-10-02 17:49:58 +0300

    testing cubes instead of images

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-interact('.dropzone').dropzone({
+/*interact('.dropzone').dropzone({
     // only accept elements matching this CSS selector
     accept: '#yes-drop',
     // Require a 75% element overlap for a drop to be possible
@@ -62,4 +62,83 @@ function dragMoveListener (event) {
     // update the posiion attributes
     target.setAttribute('data-x', x)
     target.setAttribute('data-y', y)
-}
+}*/
+
+
+//TEST DRAG&DROP SECTION
+var dragSrcEl = null;
+function handleDragStart(e) {
+    // Target (this) element is the source node.
+    this.style.opacity = '0.4';
+  
+    dragSrcEl = this;
+  
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  }
+  
+  var cols = document.querySelectorAll('#columns .column');
+  [].forEach.call(cols, function(col) {
+    col.addEventListener('dragstart', handleDragStart, false);
+  });
+
+function handleDragOver(e) {
+    if (e.preventDefault) {
+      e.preventDefault(); // Necessary. Allows us to drop.
+    }
+  
+    e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+  
+    return false;
+  }
+  
+  function handleDragEnter(e) {
+    // this / e.target is the current hover target.
+    this.classList.add('over');
+  }
+  
+  function handleDragLeave(e) {
+    this.classList.remove('over');  // this / e.target is previous target element.
+  }
+
+  function handleDrop(e) {
+    // this/e.target is current target element.
+  
+    if (e.stopPropagation) {
+      e.stopPropagation(); // Stops some browsers from redirecting.
+    }
+  
+    // Don't do anything if dropping the same column we're dragging.
+    if (dragSrcEl != this) {
+      // Set the source column's HTML to the HTML of the columnwe dropped on.
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+  
+    return false;
+  }
+  
+  function handleDragEnd(e) {
+    // this/e.target is the source node.
+  
+    [].forEach.call(cols, function (col) {
+      col.classList.remove('over');
+    });
+  }
+  
+  var cols = document.querySelectorAll('#columns .column');
+  [].forEach.call(cols, function(col) {
+    col.addEventListener('dragstart', handleDragStart, false);
+    col.addEventListener('dragenter', handleDragEnter, false)
+    col.addEventListener('dragover', handleDragOver, false);
+    col.addEventListener('dragleave', handleDragLeave, false);
+    col.addEventListener('drop', handleDrop, false);
+    col.addEventListener('dragend', handleDragEnd, false);
+  });
+
+//иконка при перетаскивании
+  var dragIcon = document.createElement('img');
+  dragIcon.src = 'logo.png';
+  dragIcon.width = 100;
+  e.dataTransfer.setDragImage(dragIcon, -10, -10);
+
