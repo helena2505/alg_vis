e6dc5159a13ee5bef079b78e7285e6ff7013f87d
helena2505 <helena2505>
2019-10-06 14:41:30 +0300

    Added a stack/queue element

--- main.js
+++ main.js
@@ -1,13 +1,55 @@
-var locMenu = document.getElementById('dynMenu');
-var lst = document.getElementById('options');
-variable.onclick = function() {
-	var li = document.createElement('LI'), li1 = document.createElement('LI'), li2 = document.createElement('LI'), li3 = document.createElement('LI');
-	li.innerHTML = 'Нарисовать';
-	lst.appendChild(li);
-	li1.innerHTML = 'Присвоить значение';
-	lst.appendChild(li1);
-	li2.innerHTML = 'Изменить значение';
-	lst.appendChild(li2);
-	li3.innerHTML = 'Поменять с другой переменной';
-	lst.appendChild(li3);
+var dragItem1 = document.getElementById("vr"); // Перетаскиваемая картинка
+var dragItem2 = document.getElementById("one_list"); // Перетаскиваемая картинка
+var dropLoc = document.getElementById("outer-dropzone"); // Канва, на которую осуществляется перетаскивание
+
+dragItem1.ondragstart = function(event) {
+    /*Функция-обработчик события начала перетаскивания картинки
+    * Функция принимает на вход событие (event)
+    * Функция записывает перетаскиваемый элемент в событие
+    * Автор: Карелина Елена
+    */
+    event.dataTransfer.setData('key', event.target.id);
+}
+
+dragItem2.ondragstart = function(event) {
+    /*Функция-обработчик события начала перетаскивания картинки
+    * Функция принимает на вход событие (event)
+    * Функция записывает перетаскиваемый элемент в событие
+    * Автор: Карелина Елена
+    */
+    event.dataTransfer.setData('key', event.target.id);
+}
+
+dropLoc.ondragover = function(event) {
+    /*Функция-обработчик события попадания картинки на канву
+    * Функция принимает на вход событие (event)
+    * Функция отключает дефолтный drag&drop
+    * Автор: Карелина Елена
+    */
+    event.preventDefault();
+}
+
+dropLoc.ondrop = function(event) {
+    /*Функция-обработчик события бросания картинки на канву
+    * Функция принимает на вход событие (event)
+    * Функция рисует картинку, идентичную перетаскиваемой, на канве
+    * и устанавливает ее размеры и координаты
+    * Автор: Карелина Елена
+    */
+    event.preventDefault();
+    var dropItem = event.dataTransfer.getData('key'); //Получение информации о перетаскиваемой картинке
+    var droppedElement = document.getElementById(dropItem);
+    var newElement = document.createElement('img'); //Создание картинки на канве
+    //Задание размеров картинки
+    newElement.style.height = "70px";
+    newElement.style.width = "100px";
+    newElement.src = droppedElement.src; //Подключение файла-источника svg
+    //Задание координат картинки
+    newElement.style.position = 'absolute';
+    newElement.style.left = event.clientX + 'px';
+    newElement.style.top = event.clientY + 'px';
+    //Задание идентификатора для картинки
+    newElement.classList.add('drag-drop');
+    newElement.id = 'var1'
+    dropLoc.appendChild(newElement); //Присоединение картинки к родительскому элементу-канве
 }
