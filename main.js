24a04f8dfd1a134cbddbf2a914a1f5a6cb20d24c
helena2505 <helena2505>
2019-11-10 21:42:30 +0300

    More comments added

--- main.js
+++ main.js
@@ -1,65 +1,53 @@
-function someFunction(firstParam, secondParam) {
-    /* The function takes two numbers
-    * The function computes the average value of these numbers
-    * The function returns the rounded value of the computed average
-    * Author: Elena Karelina
-     */
-    let average = (firstParam + secondParam)/2;
-    return Math.round(average);
-}
-
-result = someFunction(1, 2);  //Calling function which does something magic
-
 let containerMenu = document.getElementById('available-containers'); // Available containers menu
-let allPictures = containerMenu.querySelectorAll('img'); // Загрузка всех изображений, находящихся в меню
+let allPictures = containerMenu.querySelectorAll('img'); // Loading all images of the menu
 let dropLoc = document.getElementById("outer-dropzone"); // Канва, на которую осуществляется перетаскивание
 
-// Добавление к каждой картинке обработчика события начала перетаскивания
+// Adding event listener for starting drag
 for(let i = 0; i < allPictures.length; i++) {
     let curImage = allPictures[i];
     curImage.addEventListener('dragstart', transferId);
 }
 
 function transferId(event) {
-    /*Функция-обработчик события начала перетаскивания картинки
-    * Функция принимает на вход событие (event)
-    * Функция записывает перетаскиваемый элемент в событие
-    * Автор: Елена Карелина
+    /* Event listener for drag start
+    * Records the dragged element to event
+    * Input parameter: event. Output parameter: none.
+    * Author: Elena Karelina
     */
     event.dataTransfer.setData('key', event.target.id);
 }
 
 dropLoc.ondragover = function(event) {
-    /*Функция-обработчик события попадания картинки на канву
-    * Функция принимает на вход событие (event)
-    * Функция отключает дефолтный drag&drop
-    * Автор: Елена Карелина
+    /* Event listener for getting to the area of the drop zone
+    * Input parameter: event. Output parameter: none.
+    * The function disables default listener
+    * Author: Elena Karelina
     */
     event.preventDefault();
 }
 
 dropLoc.ondrop = function(event) {
-    /*Функция-обработчик события бросания картинки на канву
-    * Функция принимает на вход событие (event)
-    * Функция рисует картинку, идентичную перетаскиваемой, на канве
-    * и устанавливает ее размеры и координаты
-    * Автор: Елена Карелина
+    /* Event listener for dropping the picture to the drop zone
+    * Input parameter: event. Output parameter: none.
+    * The function creates a picture identical to the dragged one
+    * and sets its size and coordinates
+    * Input parameter: event. Output parameter: none.
     */
     event.preventDefault();
-    let dropItem = event.dataTransfer.getData('key'); //Получение информации о перетаскиваемой картинке
+    let dropItem = event.dataTransfer.getData('key'); // Getting information about the dragged picture
     let droppedElement = document.getElementById(dropItem);
-    let newElement = document.createElement('img'); //Создание картинки на канве
-    //Задание размеров картинки
+    let newElement = document.createElement('img'); // Creating new picture on the drop zone
+    // Setting size
     newElement.style.height = "70px";
     newElement.style.width = "100px";
-    newElement.src = droppedElement.src; //Подключение файла-источника svg
-    //Задание координат картинки
+    newElement.src = droppedElement.src; // Setting the image's file
+    // Setting coordinates
     console.log(droppedElement.left);
     console.log(event.pageX)
     newElement.style.left = event.pageX - newElement.offsetWidth/2 + 'px';
     newElement.style.top = event.pageY - newElement.offsetHeight/2 + 'px';
     newElement.style.position = 'absolute';
-    //Задание идентификатора для картинки
+    // Setting id for the picture
     newElement.classList.add('drag-drop');
-    dropLoc.appendChild(newElement); //Присоединение картинки к родительскому элементу-канве
+    dropLoc.appendChild(newElement); // Appending to a parent element
 }
--- modal.js
+++ modal.js
@@ -1,4 +1,4 @@
-let lst = document.getElementById('available-containers');
+let lst = document.getElementById('available-containers'); // Getting the list of containers
 let modal_1wayList = document.getElementById('list1Modal'); // Get modal element
 let allCont = lst.querySelectorAll('li'); // All containers
 let contName = document.getElementById('cont-name'); // Container name
@@ -9,27 +9,36 @@ window.addEventListener('click', outsideClick); // Listen for outside click
 
 
 lst.onclick = function(event) {
-    console.log(event.target.tagName);
+    /* Event listener for clicking on the menu
+    * The function opens modal window if the click has been on an li element
+    * Input parameter: event. Output parameter: none.
+    * Author: Elena Karelina
+     */
     if(event.target.tagName === 'LI')
         openModal(event);
 };
 
 function openModal(event) {
-    console.log(event.target.id);
-    modal_1wayList.style.display = 'block';
-    let xhr = new XMLHttpRequest(); // Создание нового HTTP запроса к серверу
-    xhr.open("POST", "include/info.php", true); // Определение типа и адреса запроса
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); // Передача кодировки информации
-    xhr.send('id=' + encodeURIComponent(event.target.id)); // Передача информации
-    xhr.onreadystatechange = function() { // Ждём ответа от сервера
-        /* Функция-обработчик события получения ответа от сервера
-        * В случае подтверждения сервером удачного добавления в БД добавляет имя контейнера в интерфейс
-        * Ничего не принимает, ничего не возвращает
-        * Автор: Елена Карелина
+    /* The function opens modal window and fills it with the information from database
+    * Input parameter: event.
+    * Output parameter: none.
+    * Author: Elena Karelina
+     */
+    let xhr = new XMLHttpRequest(); // Creating new HTTP request
+    xhr.open("POST", "include/info.php", true); // Setting destination and type
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); // Setting encoding
+    xhr.send('id=' + encodeURIComponent(event.target.id)); // Sending the container's id for which information is requested
+    xhr.onreadystatechange = function() { // Waiting for the server's answer
+        /* Event listener for getting response from server
+        * Inserts the information from server into a modal window
+        * Input parameter: event. Output parameter: none.
+        * Author: Elena Karelina
         */
-        if (xhr.readyState == 4) { // Ответ пришёл
-            if(xhr.status == 200) { // Сервер вернул код 200 (что хорошо)
+        if (xhr.readyState == 4) { // The answer has been got
+            if(xhr.status == 200) { // The server's returned code 200 (success)
+                modal_1wayList.style.display = 'block'; // Enabling visibility of modal window
                 contName.innerHTML = document.getElementById(event.target.id).textContent;
+                // Inserting information received into modal window
                 contDescr.innerHTML = xhr.responseText;
             }
         }
@@ -40,7 +49,8 @@ function closeModal() {
     /* Function has no input parameters
     * Functions closes modal
     * Function doesn't return anything
-    * Author: Shorygina Tatyana */
+    * Author: Shorygina Tatyana
+    */
     modal_1wayList.style.display = 'none';
 }
 
@@ -48,7 +58,8 @@ function outsideClick(e) {
     /* Function has no input parameters
     * Functions closes modal on outside click
     * Function doesn't return anything
-    * Author: Shorygina Tatyana */
+    * Author: Shorygina Tatyana
+    */
     if (e.target == modal_1wayList){
         modal_1wayList.style.display = 'none';
     }
