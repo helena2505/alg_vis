c768f7ff69a4662aef7a5a1ad08b1d6a63f0013b
helena2505 <elenakarelina833@gmail.com>
2020-03-05 17:03:30 +0300

    Backend for showing a scene changed

--- include/scene_show.php
+++ include/scene_show.php
@@ -1,10 +1,10 @@
 <?php
 require_once('database.php');
 $id = $_POST["id"];
-$STH = $DB->prepare("SELECT s_picture FROM scenes WHERE s_id = :id;");
+$STH = $DB->prepare("SELECT xml_code FROM scenes WHERE s_id = :id;");
 $STH->setFetchMode(PDO::FETCH_NUM);
 if(! $STH->execute(array ("id" => $id))) {
     exit(1);
 }
 $filename = $STH->fetchAll();
-echo "include/images/" . $filename[0][0];
+echo $filename[0][0];
