ea2c769087cf812481e26fac3f914367240342d6
helena2505 <elenakarelina833@gmail.con>
2020-03-05 16:09:58 +0300

    Backend for editing scene improved

--- include/render/scene_edit.php
+++ include/render/scene_edit.php
@@ -2,11 +2,10 @@
 require_once('../database.php');
 $xml = $_POST["xml"];
 $scene_id = $_POST["id"];
-$STH = $DB->prepare("SELECT update_scene(:id, :xml);");
+$STH = $DB->prepare("UPDATE scenes SET xml_code = :xml WHERE s_id = :id;");
 $STH->setFetchMode(PDO::FETCH_NUM);
 if(!$STH->execute(array ("id" => $scene_id, "xml" => $xml))) {
     echo "0";
     exit(4);
 }
-$inf = $STH->fetchAll();
 echo "1";
