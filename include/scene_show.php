764550c11949ecac8fb1d3e20d56bfcfc76112e1
helena2505 <elenakarelina833@gmail.com>
2020-03-07 11:22:17 +0300

    Getting from php array improved

--- include/scene_show.php
+++ include/scene_show.php
@@ -3,8 +3,9 @@ require_once('database.php');
 $id = $_POST["id"];
 $STH = $DB->prepare("SELECT xml_code FROM scenes WHERE s_id = :id;");
 $STH->setFetchMode(PDO::FETCH_NUM);
-if(! $STH->execute(array ("id" => $id))) {
+if(!$STH->execute(array ("id" => $id))) {
+    echo "0";
     exit(1);
 }
-$filename = $STH->fetchAll();
-echo $filename[0][0];
+$xml_code = $STH->fetchAll();
+print_r($xml_code["0"]["0"]);
