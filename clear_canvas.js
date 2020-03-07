987fefe92897c70f05f24d8339d5da2a52c75d5c
helena2505 <elenakarelina833@gmail.com>
2020-03-07 13:03:44 +0300

    Unnecessary scripts removed

--- clear_canvas.js
+++
@@ -1,7 +0,0 @@
-let resetButton = document.getElementById('no-alg');
-
-resetButton.addEventListener('click', deleteAll);
-
-function clearGraph() {
-    console.log('Here!');
-}
--- operator.php
+++ operator.php
@@ -218,7 +218,6 @@ require_once 'include/container_list.php';
     </div>
 
     <!--App's scripts-->
-    <script src="main.js" type="text/javascript"></script>
     <script src="drawio-integration/diagram-editor.js"></script>
     <script src="ncontainer.js" type="module"></script>
     <script src="context_menu.js" type="text/javascript"></script>
@@ -229,7 +228,6 @@ require_once 'include/container_list.php';
     <script src="alg_edit.js" type="text/javascript"></script>
     <script src="alg_select.js" type="text/javascript"></script>
     <script src="container_edit.js" type="text/javascript"></script>
-    <script src="clear_canvas.js"></script>
     <script src="delete_container.js"></script>
     <script src="scene_add.js" type="text/javascript"></script>
     <script src="scene_select.js" type="text/javascript"></script>
