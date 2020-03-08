8c9087db634126d1531ab6d518f935574df13016
helena2505 <elenakarelina833@gmail.com>
2020-03-08 10:57:41 +0300

    Ordering containers added

--- include/container_list.php
+++ include/container_list.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'database.php';
 
-$STH = $DB->prepare("SELECT id, container_name FROM containers");
+$STH = $DB->prepare("SELECT id, container_name FROM containers ORDER BY id;");
 $STH->setFetchMode(PDO::FETCH_ASSOC);
 $STH->execute();
 $containers = array();
