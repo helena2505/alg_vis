8dd5f783206f69a82fb2947af20b9d3dc48e644b
helena2505 <elenakarelina833@gmail.com>
2020-03-31 22:53:13 +0300

    Switching between scenes added

--- student_scene_list.js
+++ student_scene_list.js
@@ -1,5 +1,9 @@
 let algPanel = document.getElementById('structures-col');
 let algorithms  = algPanel.querySelectorAll('.one-algorithm');
+let scenes = [];
+let curScene = 0;
+let nextButton = document.getElementById('btn-next-scene');
+let prevButton = document.getElementById('btn-prev-scene');
 
 for(let i = 0; i < algorithms; i++) {
     let curAlgTree = algorithms[i];
@@ -22,15 +26,34 @@ function getScenes(event) {
         */
         if (xhr.readyState == 4) { // The answer has been got
             if (xhr.status == 200) {
-                let scenes = JSON.parse(xhr.responseText);
+                scenes = JSON.parse(xhr.responseText);
                 let canvas = document.getElementById('show-scene');
                 let curScene = document.createElement('img');
                 let code = JSON.parse(scenes[0]);
                 curScene.classList.add('editor');
+                curScene.id = 'current-scene';
                 curScene.src = code["xml_code"];
                 canvas.appendChild(curScene);
-                console.log(scenes);
+                curScene = 0;
             }
         }
     }
 }
+
+nextButton.onclick = function() {
+    if(curScene + 1 < scenes.length) {
+        curScene++;
+        let img = document.getElementById('current-scene');
+        const tmp = JSON.parse(scenes[curScene]);
+        img.src = tmp["xml_code"];
+    }
+};
+
+prevButton.onclick = function() {
+    if(curScene > 0) {
+        curScene--;
+        let img = document.getElementById('current-scene');
+        const tmp = JSON.parse(scenes[curScene]);
+        img.src = tmp["xml_code"];
+    }
+};
